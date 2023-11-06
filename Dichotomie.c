6cf2d96a6b2de0f6209b6fde75bcd6806212d5bc
HenintsoaJunior <rakotoarimananahentsa@gmail.com>
2023-11-06 05:22:01 +0100

    modification du fonction Dychotomie

--- Dichotomie.c
+++ Dichotomie.c
@@ -2,28 +2,41 @@
 #include <math.h>
 
 float f(float x);
-void displayResult(float a, float b , float ksi , float eps);
+void displayResult(float a, float b, float ksi, float eps);
+float dichotomie(float a, float b, float eps);
 
-int main(){
-	printf("Resolution de f(x)=0 par dichotomie");
-//	Donnee	
-	float a = 0 , b= 1 , // Les borne de l'intervalle de separation
-		ksi = 87,		// la solution a calculer
-		eps = 1e-6;		// la precision a atteindre
-// 	Traitement
-	
+int main() {
+    printf("Resolution de f(x)=0 par dichotomie\n");
 
-//	Resultat
+    float a = 0, b = 1, ksi, eps = 1e-6;
 
-	displayResult(a,b,ksi,esp);
-	
-	return 0;
+    ksi = dichotomie(a, b, eps);
+
+    displayResult(a, b, ksi, eps);
+
+    return 0;
 }
 
-void displayResult(float a, float b , float ksi , float eps){
-	printf("La solution dans [%g; %g] est ksi = %g a epsilon = %g ",a,b,ksi,esp);
-}
-float f(float x){	
-	return x - 2 -log(x);
+void displayResult(float a, float b, float ksi, float eps) {
+    printf("La solution dans [%g; %g] est ksi = %g a epsilon = %g\n", a, b, ksi, eps);
 }
 
+float f(float x) {
+    return x - 2 - log(x);
+}
+
+float dichotomie(float a, float b, float eps) {
+    float x;
+    while ((b - a) >= eps) {
+        x = (a + b) / 2;
+
+        if (f(x) == 0.0) {
+            return x;
+        } else if (f(a) * f(x) < 0) {
+            b = x;
+        } else {
+            a = x;
+        }
+    }
+    return (a + b) / 2;
+}
